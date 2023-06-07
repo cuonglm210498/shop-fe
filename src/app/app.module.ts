1cfa994bd3aa70e10650224001916ed3d6e7912a
cuonglm18 <cuonglm18@viettelsoftware.vn>
2023-06-07 10:46:59 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { SsssComponent } from './component/ssss/ssss.component';
 import { ContentComponent } from './component/content/content.component';
 import { FooterComponent } from './component/footer/footer.component';
 
@@ -11,7 +10,6 @@ import { FooterComponent } from './component/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SsssComponent,
     ContentComponent,
     FooterComponent
   ],
