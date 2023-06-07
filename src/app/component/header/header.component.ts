94568665ff640c92610300c0076c6e9d20514c90
cuonglm18 <cuonglm18@viettelsoftware.vn>
2023-06-07 14:58:35 +0700

    update

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  //Gửi title và imageSrc sang HTML (Property Binding)
+  public title = "Le Manh Cuong";
+  public imageSrc = "https://images.pexels.com/photos/351434/pexels-photo-351434.jpeg";
+
+  //Event Binding
+  public onButtonClick() {
+    console.log("Button Click!!!");
+  }
 }
