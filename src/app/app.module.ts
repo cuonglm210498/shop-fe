8b942b6a791e22ffbb8102f5f749b548467d2f23
cuonglm18 <cuonglm18@viettelsoftware.vn>
2023-06-07 10:44:09 +0700

    update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SsssComponent } from './component/ssss/ssss.component';
+import { ContentComponent } from './component/content/content.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SsssComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
